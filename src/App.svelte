87fcfa2568eee4c48600dc5c877a714c99cab6e7
Zeheng Li <imzehengl@gmail.com>
2020-02-07 01:37:57 -0700

    init

---
+++ src/App.svelte
@@ -0,0 +1,26 @@
+<script>
+  import "chota";
+  import {Container, Nav} from 'svelte-chota';
+
+  import HanziCard from "./components/HanziCard.svelte"
+
+  let characters = ["一", "二", "三", "四", "五", "六"]
+</script>
+
+
+<Container>
+  <Nav>
+    <a slot="center" href="/" class="brand">Hanzi Learner</a>
+  </Nav>
+
+  {#each characters as character, index}
+    <HanziCard character={character} index={index}/>
+  {/each}
+</Container>
+
+
+<style>
+  :global(:root) {
+    --grid-maxWidth: 108rem;
+  }
+</style>
