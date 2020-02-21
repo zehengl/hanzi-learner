b39cde4808d164a1a9af4ba5d5caf8b3884180bf
Zeheng Li <imzehengl@gmail.com>
2020-02-20 21:49:47 -0700

    move characaters to a seperate file

--- src/App.svelte
+++ src/App.svelte
@@ -5,32 +5,8 @@
   import HanziCard from "./components/HanziCard.svelte";
   import HanziCarousel from "./components/HanziCarousel.svelte";
   import HanziSetting from "./components/HanziSetting.svelte";
+  import characters from "./hanzi";
 
-  let characters = [
-    { zh: "一", en: "one" },
-    { zh: "二", en: "two" },
-    { zh: "三", en: "three" },
-    { zh: "人", en: "person" },
-    { zh: "个", en: "*one unit" },
-    { zh: "大", en: "big" },
-    { zh: "山", en: "mountain" },
-    { zh: "上", en: "up" },
-    { zh: "下", en: "down" },
-    { zh: "小", en: "small" },
-    { zh: "雨", en: "rain" },
-    { zh: "天", en: "sky" },
-    { zh: "车", en: "car" },
-    { zh: "开", en: "open" },
-    { zh: "火", en: "fire" },
-    { zh: "门", en: "door" },
-    { zh: "口", en: "mouth" },
-    { zh: "月", en: "moon" },
-    { zh: "羊", en: "sheep" },
-    { zh: "只", en: "*one unit" },
-    { zh: "有", en: "have" },
-    { zh: "牛", en: "cow" },
-    { zh: "水", en: "water" }
-  ];
   let options = {
     perPage: 1,
     loop: true,
